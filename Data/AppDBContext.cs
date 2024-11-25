00b81441138ae58e708dae7979ba40bf787b94f0
Keval <kevalnai1504@gmail.com>
2024-11-25 18:23:06 -0500

    Data Folder created and DBcontext added

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,12 @@
+﻿using CustomerInvoicesApp.Models;
+using Microsoft.EntityFrameworkCore;
+namespace CustomerInvoicesApp.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<InvoiceLineItem> InvoiceLineItems { get; set; }
+        public DbSet<PaymentTerms> PaymentTerms { get; set; }
+    }
+}
