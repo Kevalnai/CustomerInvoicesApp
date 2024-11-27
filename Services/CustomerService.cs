14bfb4b450d27bd3b4f8f46f19cd5b9140d53058
Keval <kevalnai1504@gmail.com>
2024-11-27 06:24:11 -0500

    Test Run Done 3/4 success

---
+++ Services/CustomerService.cs
@@ -0,0 +1,32 @@
+﻿using CustomerInvoicesApp.Data;
+using CustomerInvoicesAppLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerInvoicesApp.Services
+{
+    public class CustomerService
+    {
+        private readonly AppDBContext _context;
+
+        public CustomerService(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Customer> GetCustomerByIdAsync(int id)
+        {
+            return await _context.Customers.FindAsync(id);
+        }
+
+        public async Task<List<Customer>> GetAllCustomersAsync()
+        {
+            return await _context.Customers.Where(c => !c.IsDeleted).ToListAsync();
+        }
+
+        public async Task AddCustomerAsync(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
